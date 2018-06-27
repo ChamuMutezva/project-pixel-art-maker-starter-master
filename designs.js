ef0973ac8920112b872d10a551baea4c4451c010
Chamu Mutezva <ckmutezva@gmail.com>
2018-06-27 05:23:02 +0200

    css changes

--- designs.js
+++ designs.js
@@ -1,47 +1,38 @@
-// Select color input
 // Select size input
-
-// When size is submitted by the user, call makeGrid()
 let height = document.getElementById("inputHeight");
 let width = document.getElementById("inputWidth");
+// Select color input
 let color = document.getElementById("colorPicker");
+//this is the table
 let canvas = document.getElementById("pixelCanvas");
 
-console.log(height.value);
-console.log(width.value);
-
-
+// When size is submitted by the user, call makeGrid()
 function makeGrid() {
-
-    // Your code goes here!
-    for(let r = 0; r < height.value; r++){
+    for (let r = 0; r < height.value; r++) {
         const row = canvas.insertRow(r);
-        for(let c = 0;c < width.value; c++){
-             row.insertCell(c);
-            //canvas.appendChild(row);
-           // canvas.appendChild(cell);
-            //cell.on("click", fillSquare);
+        for (let c = 0; c < width.value; c++) {
+            row.insertCell(c);
         }
-
     }
-    $("table").on("click", function(){
+    // click on a square to change the color of a particular square
+    $("table").on("click", function () {
         $(event.target).css("background-color", color.value);
     })
-
-
 }
-
+//function to clear the table before creating a new one
 function clearGrid() {
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
 }
-$("#button").on("click", function(event){
- event.preventDefault();
-  clearGrid() ;
-  console.log( "button clicked");
-  console.log(height.value);
-  console.log(width.value);
-  console.log(color.value);
-  makeGrid();
+
+$("#button").on("click", function (event) {
+    event.preventDefault();
+    clearGrid();
+    //debugging. Check if inputs are being captured correctly
+    console.log("button clicked");
+    console.log(height.value);
+    console.log(width.value);
+    console.log(color.value);
+    makeGrid();
 })
