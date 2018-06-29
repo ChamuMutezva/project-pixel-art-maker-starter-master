c9c30ec321cae600c0c75d0133cf92ae7c534a4a
Chamu Mutezva <ckmutezva@gmail.com>
2018-06-29 08:41:52 +0200

    alcProChange

--- designs.js
+++ designs.js
@@ -1,22 +1,27 @@
-// Select size input
-let height = document.getElementById("inputHeight");
-let width = document.getElementById("inputWidth");
-// Select color input
-let color = document.getElementById("colorPicker");
 //this is the table
-let canvas = document.getElementById("pixelCanvas");
-
+let canvas = document.getElementById('pixelCanvas');
+// Select color input
+let color = document.getElementById('colorPicker');
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
+    // Select size input
+    let height = document.getElementById('inputHeight');
+    let width = document.getElementById('inputWidth');
+
     for (let r = 0; r < height.value; r++) {
         const row = canvas.insertRow(r);
         for (let c = 0; c < width.value; c++) {
             row.insertCell(c);
         }
     }
-    // click on a square to change the color of a particular square
-    $("table").on("click", function () {
-        $(event.target).css("background-color", color.value);
+    //click on a square to change the color of a particular square
+    $('table').on('click', function (event) {
+        
+       //$(event.target).toggle(
+           $(event.target).css({'background-color': color.value});
+          // function(){$(event.target).removeAttr('style')}
+      // )          
+    
     })
 }
 //function to clear the table before creating a new one
@@ -26,13 +31,8 @@ function clearGrid() {
     }
 }
 
-$("#button").on("click", function (event) {
+$('#button').on('click', function (event) {
     event.preventDefault();
     clearGrid();
-    //debugging. Check if inputs are being captured correctly
-    console.log("button clicked");
-    console.log(height.value);
-    console.log(width.value);
-    console.log(color.value);
     makeGrid();
 })
